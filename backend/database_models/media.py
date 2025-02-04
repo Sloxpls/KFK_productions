cff338649bff41c3a7aa53e955c7d10220fd831f
Mustomog <mustomog@example.com>
2025-02-04 21:01:29 +0900

    filename -> name

--- backend/database_models/media.py
+++ backend/database_models/media.py
@@ -4,7 +4,7 @@ class Media(db.Model):
     __tablename__ = 'media'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    filename = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     file_path = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=True)
     uploaded_at = db.Column(db.DateTime, default=db.func.current_timestamp())
