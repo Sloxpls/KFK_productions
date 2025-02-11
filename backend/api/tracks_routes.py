3e4e311de169d9f22c833776f35e8a7acaff4f45
jim <jim.sj18@gmail.com>
2025-02-11 02:36:08 +0100

    add img downlowd and

--- backend/api/tracks_routes.py
+++ backend/api/tracks_routes.py
@@ -11,24 +11,21 @@ track_bp = Blueprint('track_bp', __name__)
 @track_bp.route('/tracks', methods=['GET'])
 def get_tracks():
     tracks = Track.query.all()
-    data = []
-    for t in tracks:
-        data.append({
-            'id': t.id,
-            'title': t.title,
-            'description': t.description,
-            'song_path': t.song_path,
-            'img_path': t.img_path,
-            'producer': t.producer,
-            'writer': t.writer,
-            'genre': t.genre,
-            'tiktok': t.tiktok,
-            'soundcloud': t.soundcloud,
-            'spotify': t.spotify,
-            'youtube': t.youtube,
-            'instagram': t.instagram
-        })
-    return jsonify(data), 200
+    return jsonify([{
+        'id': t.id,
+        'title': t.title,
+        'description': t.description,
+        'song_path': t.song_path,
+        'img_path': t.img_path,
+        'producer': t.producer,
+        'writer': t.writer,
+        'genre': t.genre,
+        'tiktok': t.tiktok,
+        'soundcloud': t.soundcloud,
+        'spotify': t.spotify,
+        'youtube': t.youtube,
+        'instagram': t.instagram
+    } for t in tracks]), 200
 
 @track_bp.route('/tracks/<int:track_id>', methods=['GET'])
 def get_track(track_id):
@@ -44,24 +41,63 @@ def get_track(track_id):
         'producer': track.producer,
         'writer': track.writer,
         'genre': track.genre,
+        'tiktok': track.tiktok,
+        'soundcloud': track.soundcloud,
+        'spotify': track.spotify,
+        'youtube': track.youtube,
+        'instagram': track.instagram
     }), 200
 
 @track_bp.route('/tracks', methods=['POST'])
 def create_track():
-    req_data = request.get_json() or {}
-    new_track = Track(
-        title=req_data.get('title'),
-        description=req_data.get('description'),
-        song_path=req_data.get('song_path'),
-        img_path=req_data.get('img_path'),
-        producer=req_data.get('producer'),
-        writer=req_data.get('writer'),
-        genre=req_data.get('genre'),
+    if 'song' in request.files or 'image' in request.files:
+        # Handle multipart/form-data
+        song_file = request.files.get('song')
+        image_file = request.files.get('image')
+        title = request.form.get('title', '')
+        description = request.form.get('description', '')
+        producer = request.form.get('producer')
+        writer = request.form.get('writer')
+        genre = request.form.get('genre')
 
-    )
-    db.session.add(new_track)
-    db.session.commit()
-    return jsonify({'message': 'Track created', 'id': new_track.id}), 201
+        song_filename = None
+        if song_file:
+            song_filename = secure_filename(song_file.filename)
+            song_file.save(os.path.join(UPLOAD_FOLDER, song_filename))
+
+        img_filename = None
+        if image_file:
+            img_filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(UPLOAD_FOLDER, img_filename))
+
+        new_track = Track(
+            title=title or 'Untitled',
+            description=description,
+            song_path=song_filename,
+            img_path=img_filename,
+            producer=producer,
+            writer=writer,
+            genre=genre
+        )
+        db.session.add(new_track)
+        db.session.commit()
+        return jsonify({'message': 'Track created', 'id': new_track.id}), 201
+
+    else:
+        # Handle JSON
+        req_data = request.get_json() or {}
+        new_track = Track(
+            title=req_data.get('title', 'Untitled'),
+            description=req_data.get('description', ''),
+            song_path=req_data.get('song_path'),
+            img_path=req_data.get('img_path'),
+            producer=req_data.get('producer'),
+            writer=req_data.get('writer'),
+            genre=req_data.get('genre'),
+        )
+        db.session.add(new_track)
+        db.session.commit()
+        return jsonify({'message': 'Track created', 'id': new_track.id}), 201
 
 @track_bp.route('/tracks/<int:track_id>', methods=['PUT'])
 def update_track(track_id):
@@ -69,54 +105,67 @@ def update_track(track_id):
     if not track:
         return jsonify({'error': 'Track not found'}), 404
 
-    req_data = request.get_json() or {}
-    track.title = req_data.get('title', track.title)
-    track.description = req_data.get('description', track.description)
-    track.song_path = req_data.get('song_path', track.song_path)
-    track.img_path = req_data.get('img_path', track.img_path)
-    track.producer = req_data.get('producer', track.producer)
-    track.writer = req_data.get('writer', track.writer)
-    track.genre = req_data.get('genre', track.genre)
+    if 'song' in request.files or 'image' in request.files:
+        # Handle multipart/form-data
+        song_file = request.files.get('song')
+        image_file = request.files.get('image')
+        track.title = request.form.get('title', track.title)
+        track.description = request.form.get('description', track.description)
+        track.producer = request.form.get('producer', track.producer)
+        track.writer = request.form.get('writer', track.writer)
+        track.genre = request.form.get('genre', track.genre)
 
-    db.session.commit()
-    return jsonify({'message': 'Track updated', 'id': track.id}), 200
+        if song_file:
+            new_song_filename = secure_filename(song_file.filename)
+            song_file.save(os.path.join(UPLOAD_FOLDER, new_song_filename))
+            track.song_path = new_song_filename
+
+        if image_file:
+            new_img_filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(UPLOAD_FOLDER, new_img_filename))
+            track.img_path = new_img_filename
+
+        db.session.commit()
+        return jsonify({'message': 'Track updated', 'id': track.id}), 200
+
+    else:
+        # Handle JSON
+        req_data = request.get_json() or {}
+        track.title = req_data.get('title', track.title)
+        track.description = req_data.get('description', track.description)
+        track.song_path = req_data.get('song_path', track.song_path)
+        track.img_path = req_data.get('img_path', track.img_path)
+        track.producer = req_data.get('producer', track.producer)
+        track.writer = req_data.get('writer', track.writer)
+        track.genre = req_data.get('genre', track.genre)
+        db.session.commit()
+        return jsonify({'message': 'Track updated', 'id': track.id}), 200
 
 @track_bp.route('/tracks/<int:track_id>', methods=['DELETE'])
 def delete_track(track_id):
     track = Track.query.get(track_id)
     if not track:
         return jsonify({'error': 'Track not found'}), 404
-
     db.session.delete(track)
     db.session.commit()
     return jsonify({'message': 'Track deleted', 'id': track.id}), 200
-
 
 @track_bp.route('/tracks/<int:track_id>/song', methods=['GET'])
 def get_song(track_id):
     track = Track.query.get(track_id)
     if not track or not track.song_path:
         return jsonify({'error': 'Song not found'}), 404
-
     full_path = os.path.join(UPLOAD_FOLDER, track.song_path)
     if not os.path.exists(full_path):
         return jsonify({'error': 'Song file does not exist'}), 404
-
     return send_file(full_path, mimetype='audio/mpeg')
 
-
-#
-# 7) SERVE THE IMAGE FILE
-#
 @track_bp.route('/tracks/<int:track_id>/image', methods=['GET'])
 def get_image(track_id):
     track = Track.query.get(track_id)
     if not track or not track.img_path:
         return jsonify({'error': 'Image not found'}), 404
-
     full_path = os.path.join(UPLOAD_FOLDER, track.img_path)
     if not os.path.exists(full_path):
         return jsonify({'error': 'Image file does not exist'}), 404
-
-    # Use an appropriate MIME type or guess it
     return send_file(full_path, mimetype='image/jpeg')
