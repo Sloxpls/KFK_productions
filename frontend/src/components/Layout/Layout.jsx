dbf4cf5cdf77a5cdb68f6db083503411e1e1ea46
Mustomog <mustomog@example.com>
2025-02-12 14:42:29 +0900

    moved fetching logic to custom hook

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,33 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
 
 import AudioPlayer from "./AudioPlayer.jsx";
+import useTracks from "../../hooks/useTracks.js";
 
 import Header from "./Header.jsx";
 import "./Layout.css";
 
-const fetchTracks = async () => {
-  try {
-    const response = await fetch("/api/tracks");
-    if (!response.ok) {
-      throw new Error(`Tracks API error: ${response.status}`);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    alert("Error fetching data. Please try again later.");
-  }
-}
-
 const Layout = () => {
-  const { data: tracks, refetch: refreshTracks} = useQuery({
-    queryKey: ["tracks"],
-    queryFn: fetchTracks,
-    initialData: [],
-    refetchOnWindowFocus: false,
-    refetchOnMount: true,
-    refetchOnReconnect: true,
-  });
+  const { tracks, refreshTracks } = useTracks();
 
   return (
     <>
