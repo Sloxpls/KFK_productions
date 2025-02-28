076ac32ff263b8f383d627b2de94b034bfeb9ab0
jim <jim.sj18@gmail.com>
2025-02-28 05:44:48 +0100

    add new colums to playlist table

--- backend/database_models/playlist.py
+++ backend/database_models/playlist.py
@@ -6,9 +6,14 @@ class Playlist(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
 
+    status = db.Column(db.Integer, nullable=True, default=1)
+    description = db.Column(db.Text, nullable=True)
+    img_path = db.Column(db.String(255), nullable=True)
+
+
     tracks = db.relationship(
         'Track',
         secondary='playlist_tracks',
         backref=db.backref('playlists', lazy='dynamic'),
-        lazy='dynamic'  # Query tracks lazily
+        lazy='dynamic'
     )
