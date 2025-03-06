4826ececb48e48c5adfb448eab7fa20bd999cae3
jim <jim.sj18@gmail.com>
2025-03-07 00:07:59 +0100

    add docker volumes

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,36 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from flask_migrate import Migrate
+from backend.database_models import db
+from api import track_bp, media_bp, playlist_bp, auth_bp, upload_bp, system_info_bp
 
 load_dotenv()
-from flask import Flask
-from api import track_bp, media_bp, playlist_bp, auth_bp, upload_bp, system_info_bp
-from backend.database_models import db
 
+migrate = Migrate()  # Initialize Migrate globally
 
 def create_app():
     app = Flask(__name__)
 
+    # --- Configuration ---
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'defaultsecret')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
-    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # --- Initialize Extensions ---
     db.init_app(app)
+    migrate.init_app(app, db)  # Attach Flask-Migrate
+
+    # --- Register Blueprints ---
     app.register_blueprint(media_bp)
     app.register_blueprint(track_bp)
     app.register_blueprint(playlist_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(upload_bp)
     app.register_blueprint(system_info_bp)
-    with app.app_context():
-        db.create_all()
 
     return app
 
-
 if __name__ == '__main__':
     app = create_app()
-    print(app.url_map)
     app.run(host='0.0.0.0', port=5000, debug=True)
