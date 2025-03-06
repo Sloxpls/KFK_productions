77ba8281661539e3fb1ee6db157b3ecbe8ac300a
jim <jim.sj18@gmail.com>
2025-03-06 11:04:39 +0100

    add migrateion

--- backend/app.py
+++ backend/app.py
@@ -6,17 +6,18 @@ load_dotenv()
 from flask import Flask
 from api import track_bp, media_bp, playlist_bp, auth_bp, upload_bp, system_info_bp
 from backend.database_models import db
-from flask_migrate import Migrate, upgrade
+from flask_migrate import Migrate
 
 
 def create_app():
     app = Flask(__name__)
-    migrate = Migrate()
 
     # --- App Config ---
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'defaultsecret')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 
     # --- Register Blueprints ---
     app.register_blueprint(media_bp)
@@ -26,20 +27,11 @@ def create_app():
     app.register_blueprint(upload_bp)
     app.register_blueprint(system_info_bp)
 
+
+
     # --- Initialize Flask Extensions ---
     db.init_app(app)
-    migrate.init_app(app, db)
-
-    # --- Run DB migrations automatically before first request ---
-    @app.before_first_request
-    def run_migrations():
-        with app.app_context():
-            try:
-                upgrade()
-                print("Database migrations applied.")
-            except Exception as e:
-                print(f"Migration failed: {e}")
-
+    migrate = Migrate(app, db, command='migrate')
     return app
 
 
