fda56ca69da00ae8ac1f712367f146b9b2ea24e1
jim <jim.sj18@gmail.com>
2025-03-06 21:40:55 +0100

    vet ej

--- backend/app.py
+++ backend/app.py
@@ -2,43 +2,27 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 from flask import Flask
 from api import track_bp, media_bp, playlist_bp, auth_bp, upload_bp, system_info_bp
 from backend.database_models import db
-from flask_migrate import Migrate, upgrade
 
 
 def create_app():
     app = Flask(__name__)
-    migrate = Migrate()
 
-    # --- App Config ---
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'defaultsecret')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # --- Register Blueprints ---
+    db.init_app(app)
     app.register_blueprint(media_bp)
     app.register_blueprint(track_bp)
     app.register_blueprint(playlist_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(upload_bp)
     app.register_blueprint(system_info_bp)
-
-    # --- Initialize Flask Extensions ---
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # --- Run DB migrations automatically before first request ---
-    @app.before_first_request
-    def run_migrations():
-        with app.app_context():
-            try:
-                upgrade()
-                print("Database migrations applied.")
-            except Exception as e:
-                print(f"Migration failed: {e}")
+    with app.app_context():
+        db.create_all()
 
     return app
 
